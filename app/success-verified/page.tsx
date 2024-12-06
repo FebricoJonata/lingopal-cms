1d6008ce40c8928dc4c28a03296e89323611b552
FebricoJonata <febrico.jonata@kawanlamaretail.com>
2024-12-06 15:16:26 +0700

    style: make success verified page responsive on mobile

--- app/success-verified/page.tsx
+++ app/success-verified/page.tsx
@@ -7,11 +7,14 @@ import { Button } from "@/components/ui/button";
 const SuccessVerified = () => {
   return (
     <div className="flex flex-col justify-center items-center h-screen ">
-      <Lottie animationData={Success} className="w-[50%] h-[50%]" />
-      <h1 className="text-4xl text-primary font-bold">
+      <Lottie
+        animationData={Success}
+        className="lg:w-[50%] lg:h-[50%] w-[30%] h-[30%]"
+      />
+      <h1 className="lg:text-4xl text-2xl  text-primary font-bold">
         Email successfully Verified!
       </h1>
-      <p className="text-gray-500 text-center max-w-md mt-2 mb-4">
+      <p className="text-gray-500 text-center lg:max-w-md px-4 mt-2 mb-4 lg:text-md text-sm">
         Your email has been successfully verified. You can now access all
         features of your account without any restrictions. Thank you for
         verifying your email!
